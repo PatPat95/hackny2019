aac51514528a36fe9e32fae50b4ede386b9bf845
Alan Lau <alanlau97@gmail.com>
2019-09-28 19:05:55 -0400

    reconstruct

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter as Router, Link, NavLink, Redirect} from 'react-router-dom';
 import Route from 'react-router-dom/Route'
 import routes from './routes'
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
-//seomthing
 Amplify.configure(awsconfig);
 
-<<<<<<< HEAD
 const User = (params) => {
   return ( <h1> Welcome User {params.username} </h1>)
 }
 
 class App extends Component {
-
   render() {
     return (
       <Router>
@@ -48,16 +43,6 @@ class App extends Component {
       </Router>
     );
   } 
-=======
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Testing shit</h1>
-      </div>
-    );
-  }
->>>>>>> 04851253088eaff38bdd3354b2c1bccff8b2fdb9
 }
 
-export default App;
+export default withAuthenticator(App, true);
