9bdfe3d9ac8c3a2c4130a51537f0ad8741097def
Kaizen Conroy <kaizenconroy@172-29-209-201.dynapool.nyu.edu>
2019-09-28 18:18:37 -0400

    auth

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Link, NavLink, Redirect} from 'react-router-dom';
 import Route from 'react-router-dom/Route'
+import routes from './routes'
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
+
+Amplify.configure(awsconfig);
 
 const User = (params) => {
   return ( <h1> Welcome User {params.username} </h1>)
@@ -48,5 +54,4 @@ class App extends Component {
     );
   } 
 }
-
-export default App;
+export default withAuthenticator(App, true);
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+// import {card,signin,signup} from './pages ';
+
+// const routes = (
+//     <Router>
+//       <Route exact path="/signup" component = {signup}/>
+//       <Route exact path="/signin" component = {signin}/>
+//       <Route exact path="/card" component = {card}/>
+//     </Router>
+// )
+
+// export default routes
