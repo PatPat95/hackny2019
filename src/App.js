003e0960cde6dde0addc7c9522d1b523dc4ec979
Kaizen Conroy <kaizenconroy@172-29-209-201.dynapool.nyu.edu>
2019-09-28 16:23:39 -0400

    actual path

--- src/App.js
+++ src/App.js
@@ -7,23 +7,17 @@ import Route from 'react-router-dom/Route'
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <Router>
       <div classname="App">
         <h1>Testing push</h1>
-        <Route path ="/" render = {()=> {
+        <Route path ="/" exact render = {()=> {
           return ( <h1>Welcome Home</h1>)
+        }} />
+        <Route path ="/settings" exact render = {()=> {
+          return ( <h1>Settings </h1>)
         }} />
       </div>
       </Router>
-=======
-      <menu>
-        <Menu.Item>Menu</Menu.Item>
-        <SubMenu title="SubMenu">
-          <Menu.Item>SubMenuItem</Menu.Item>
-        </SubMenu>
-      </menu>
->>>>>>> b2052a4d9646f32ac5f31602d9bab473bfdbbfc6
     );
   } 
 }
