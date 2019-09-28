d4e546cdf8ec18afa0ffb23e49d9ffb7dc8e07c9
Kaizen Conroy <kaizenconroy@172-29-209-201.dynapool.nyu.edu>
2019-09-28 16:57:24 -0400

    routing stuff

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Link, NavLink, Redirect} from 'react-router-dom';
 import Route from 'react-router-dom/Route'
 
+const User = (params) => {
+  return ( <h1> Welcome User {params.username} </h1>)
+}
+
 class App extends Component {
+  state = {
+    loggedIn:false
+  }
+  loginHandle = () => {
+    this.setState({loggedIn:true})
+  }
+
   render() {
     return (
       <Router>
       <div classname="App">
-        <h1>Testing push</h1>
+      <ul>
+        <li>
+        <NavLink to="/" exact activeStyle= {{color:'green'}}>Home</NavLink>
+        </li>
+        <li>
+        <NavLink to="/settings" exact activeStyle= {{color:'green'}}>Settings</NavLink>
+        </li>
+        <li>
+        <NavLink to="/user/John" exact activeStyle= {{color:'green'}}>User John</NavLink>
+        </li>
+      </ul>
+
+      <input type="button" value = "log in" onClick = {this.loginHandle.bind(this)}/>
         <Route path ="/" exact render = {()=> {
           return ( <h1>Welcome Home</h1>)
         }} />
         <Route path ="/settings" exact render = {()=> {
           return ( <h1>Settings </h1>)
         }} />
+        <Route path ="/user/:username" exact render={({match}) =>(
+          this.state.loggedIn ? (<User username = {match.params.username}
+          />) : (<Redirect to ="/" /> )
+        )} />
       </div>
       </Router>
     );
