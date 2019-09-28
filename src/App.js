cbd1464cc400e7f9ea9e65ccbf22ae645e27649f
Kaizen Conroy <kaizenconroy@172-29-209-201.dynapool.nyu.edu>
2019-09-28 16:17:38 -0400

    added some router stuff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router} from 'react-router-dom';
+import Route from 'react-router-dom/Route'
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div classname="App">
         <h1>Testing push</h1>
+        <Route path ="/" render = {()=> {
+          return ( <h1>Welcome Home</h1>)
+        }} />
       </div>
+      </Router>
     );
-  }
+  } 
 }
 
 export default App;
