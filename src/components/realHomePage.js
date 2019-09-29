9ec0786e34ec5ea680e33c743e41ab3fc8904cb3
Kaizen Conroy <kaizenconroy@172-29-209-201.dynapool.nyu.edu>
2019-09-29 01:22:26 -0400

    1:22

--- src/components/realHomePage.js
+++ src/components/realHomePage.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import CollectionsPage from './addContact';
+import { Auth } from 'aws-amplify';
+
+
+Auth.currentAuthenticatedUser({
+    bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+ }).then(user => console.log(user))
+ .catch(err => console.log(err));
+
 
 class RealHomePage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: ''
+        }
+    }
+    componentWillMount() {
+        Auth.currentAuthenticatedUser({
+            bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+         }).then(user => this.setState({username: user.username}))
+         .catch(err => console.log(err));
+    }
+
     render () {
+        
         return(
             <div>
-                <h1>This is the homepage of hackny-f2019 project</h1>
+                {<CollectionsPage
+                    showModal = {this.showModal}
+                    handleCancel = {this.handleCancel}
+                    handleCreate = {this.handleCreate}
+                    visible = {this.state.visible}
+                    saveFormRef={this.saveFormRef}
+                />}
+                <div>
+                    user's name: {this.state.username}
+                </div>
             </div>
         )
     }
