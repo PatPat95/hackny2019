c4c016e659f8b7d04632c597586004d4fc7db462
Alan Lau <alanlau97@gmail.com>
2019-09-29 03:36:31 -0400

    redirect

--- src/components/menu.js
+++ src/components/menu.js
@@ -6,28 +6,41 @@ import CropFreeIcon from '@material-ui/icons/CropFree';
 import RecentActorsIcon from '@material-ui/icons/RecentActors';
 import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
 
+import { Redirect, Link } from 'react-router-dom'
+
+// Auth.currentAuthenticatedUser({
+//    bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+// }).then(user => console.log(user))
+// .catch(err => console.log(err));
+
 const useStyles = makeStyles({
   root: {
     width: 500,
   },
 });
 
-export default function SimpleBottomNavigation() {
+export default function SimpleBottomNavigation(props) {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
+
   return (
+    <div>
+    
     <BottomNavigation
       value={value}
       onChange={(event, newValue) => {
-        setValue(newValue);
+        setValue(newValue)
       }}
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction label="My Cards" icon={<AccountBalanceWalletIcon />} />
-      <BottomNavigationAction label="Collected Cards" icon={<RecentActorsIcon />} />
-      <BottomNavigationAction label="Scan"icon={<CropFreeIcon />} />
+      {value ==='a' ? <Redirect to={"/user/"+props.userId} /> : null}
+      {value ==='c' ? <Redirect to="/webcam" /> : null}
+      <BottomNavigationAction label="My Cards" icon={<AccountBalanceWalletIcon />} value="a"/>
+      <BottomNavigationAction label="Collected Cards" icon={<RecentActorsIcon />} value="b"/>
+      <BottomNavigationAction label="Scan"icon={<CropFreeIcon />} value="c"/>
     </BottomNavigation>
+    </div>
   );
 }
---
+++ src/page/webcam.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Webcam extends React.Component{
+    render() {
+        return(
+            <div>
+                <h1>webcam</h1>
+            </div>
+        )
+    }
+}
+
+export default Webcam;
